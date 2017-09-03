693bde6d8e00002cb41b8a0dc34704c15bb0ef56
Gary Tierney <gary.tierney@gmx.com>
2017-09-03 02:42:07 +0100

    Add top-level documentation to libsecsp

--- libsecsp/src/lib.rs
+++ libsecsp/src/lib.rs
@@ -1,3 +1,22 @@
+//! This is the parsing backend library for the CSP language.
+//!
+//! The top level `parse` function can be used to parse a list of
+//! statements from an input source implementing `Read`.  The parser combinators
+//! that this library is built on are exposed under the `parser` module, and AST types
+//! under the `syntax` module.
+//!
+//! # Examples
+//! ```rust
+//! use secsp::syntax::*;
+//!
+//! let input = b"block abc{}";
+//! let parse_result = secsp::parse_from_slice(&input[..]);
+//! match parse_result.statements[0] {
+//!     Statement::Declaration(Declaration::Block { is_abstract, ref name, ref qualifier, ref statements }) => println!("Parsed block"),
+//!     _ => panic!("Didn't find a block!")
+//! }
+//! ```
+
 #![crate_name = "secsp"]
 #![crate_type = "lib"]
 
@@ -5,7 +24,7 @@
 extern crate nom;
 
 pub mod parser;
-pub mod syntax; 
+pub mod syntax;
 
 pub struct ParseResult {
     pub statements: Vec<syntax::Statement>,
@@ -18,7 +37,7 @@ pub fn parse<R: std::io::Read>(input: &mut R) -> ParseResult {
     parse_from_slice(&buffer)
 }
 
-fn parse_from_slice(input: &[u8]) -> ParseResult {
+pub fn parse_from_slice(input: &[u8]) -> ParseResult {
     let (_, result) = parser::statement_list(input).unwrap();
 
     ParseResult { statements: result }
