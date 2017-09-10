c055e7cfa0e43bef2c94d36e378ed771106f2405
Gary Tierney <gary.tierney@gmx.com>
2017-09-11 00:41:01 +0100

    Fix smoke test build

--- smoke-tests.sh
+++ smoke-tests.sh
@@ -3,12 +3,6 @@
 set -e
 
 SCRIPTPATH=`pwd`
-echo "$SCRIPTPATH"
-ls "$SCRIPTPATH" "$SCRIPTPATH/target" $SCRIPTPATH/target/*
-
-if [ -f "$SCRIPTPATH/smoke-tests.sh" ]; then
-    echo "Smoke-tests.sh exists"
-fi
 
 if [ -z "$CSPC" ]; then 
     CSPC="$SCRIPTPATH/target/debug/cspc"
