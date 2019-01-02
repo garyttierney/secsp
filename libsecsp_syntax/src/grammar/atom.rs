2e3ff9342d50c025fb9edc8a39336879f38a83ce
Gary Tierney <gary.tierney@fastmail.com>
2019-01-02 23:17:13 +0000

    Add tests for parsing atom expressions

--- libsecsp_syntax/src/grammar/atom.rs
+++ libsecsp_syntax/src/grammar/atom.rs
@@ -7,7 +7,12 @@ use crate::token::TokenType;
 
 pub fn path_expr(p: &mut CspParser) -> CompletedMarker<SyntaxKind, Token> {
     let m = p.mark();
-    p.expect_one_of(vec![TokenType::Dot, TokenType::Name]);
+
+    if p.at(TokenType::Dot) {
+        p.bump();
+    }
+
+    p.expect(TokenType::Name);
 
     while p.at(TokenType::Dot) {
         p.bump();
@@ -57,4 +62,33 @@ pub fn literal_expr(p: &mut CspParser) -> CompletedMarker<SyntaxKind, Token> {
 pub fn is_at_path_start(p: &CspParser, offset: usize) -> bool {
     let tok = p.nth(offset);
     tok == SyntaxKind::Token(TokenType::Dot) || tok == SyntaxKind::Token(TokenType::Name)
+}
+
+
+#[test]
+fn parse_global_path() {
+    crate::grammar::test::test_parser(r#"
+        callstub(<marker type="pathexpr">.global.item</marker>);
+    "#)
+}
+
+#[test]
+fn parse_nested_path() {
+    crate::grammar::test::test_parser(r#"
+        callstub(<marker type="pathexpr">nested1.nested2.nested3</marker>);
+    "#)
+}
+
+#[test]
+fn parse_list_expr() {
+    crate::grammar::test::test_parser(r#"
+        callstub(<marker type="listexpr">(item1, item2, item3)</marker>);
+    "#)
+}
+
+#[test]
+fn parse_paren_expr() {
+    crate::grammar::test::test_parser(r#"
+        callstub(<marker type="parenexpr">(a && b)</marker>);
+    "#)
 }
