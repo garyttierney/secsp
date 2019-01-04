6c65c6e4fddcc586f666718e8efcf6a2f9fe46e0
Gary Tierney <gary.tierney@fastmail.com>
2019-01-04 06:19:11 +0000

    Fix crashes found by AFL

--- libsecsp_syntax/src/lib.rs
+++ libsecsp_syntax/src/lib.rs
@@ -1,11 +1,13 @@
 extern crate drop_bomb;
+extern crate itertools;
 extern crate logos;
 extern crate rowan;
 extern crate smol_str;
 extern crate text_unit;
 
 pub mod ast;
+pub mod parser;
+
 mod grammar;
 mod lexer;
-pub mod parser;
 mod token;
