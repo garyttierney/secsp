93c01e17633b2aeef1a8187c7116eea184805396
Gary Tierney <gary.tierney@fastmail.com>
2019-05-05 03:54:41 +0100

    Add debug statement to test coverage

--- tools/scripts/run-tests-with-coverage.sh
+++ tools/scripts/run-tests-with-coverage.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd)"
+echo "$SCRIPT_PATH"
+echo `pwd`
 ROOT_PATH="$SCRIPT_PATH/../.."
 CONVERTER_PATH="$ROOT_PATH/tools/coverage-converter/lcov_cobertura.py"
 
@@ -10,7 +12,7 @@ export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-c
 set -x
 cargo +nightly clean
 cargo +nightly build --verbose
-cargo +nightly test --verbose
+cargo +nightly test --verbose --format json > test_results.json
 
 zip -0 ccov.zip `find . \( -name "secsp*.gc*" \) -print`;
 grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore-dir "/*" -o lcov.info;
