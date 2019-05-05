5f0afb0af02cc55b6ed0b4f9ee0dc3784b514948
Gary Tierney <gary.tierney@fastmail.com>
2019-05-05 04:20:31 +0100

    Continue publishing coverage to codecov

--- tools/scripts/run-tests-with-coverage.sh
+++ tools/scripts/run-tests-with-coverage.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd)"
-echo "$SCRIPT_PATH"
-echo `pwd`
 ROOT_PATH="$SCRIPT_PATH/../.."
 CONVERTER_PATH="$ROOT_PATH/tools/coverage-converter/lcov_cobertura.py"
 
@@ -14,6 +12,7 @@ cargo +nightly clean
 cargo +nightly build --verbose
 cargo +nightly test --verbose --format json > test_results.json
 
+
 zip -0 ccov.zip `find . \( -name "secsp*.gc*" \) -print`;
 grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore-dir "/*" -o lcov.info;
-python "$CONVERTER_PATH" lcov.info
+bash <(curl -s https://codecov.io/bash) -t "${CODECOV_TOKEN}";
