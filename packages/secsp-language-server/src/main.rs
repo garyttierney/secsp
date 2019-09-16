523ff973745225370ca777c1ddb0cc811697d126
Gary Tierney <gary.tierney@fastmail.com>
2019-09-16 02:08:43 +0100

    Use tower-lsp with tokio instead of lsp-server

--- packages/secsp-language-server/src/main.rs
+++ packages/secsp-language-server/src/main.rs
@@ -1,38 +1,123 @@
-extern crate crossbeam_channel;
-extern crate env_logger;
-extern crate failure;
-extern crate lsp_server;
-extern crate log;
-extern crate lsp_types;
-extern crate secsp_analysis;
-extern crate secsp_language_server;
+extern crate futures;
+extern crate jsonrpc_core;
+extern crate serde_json;
+extern crate tower_lsp;
+extern crate tokio;
 
-use crossbeam_channel::{Receiver, Sender};
-use env_logger::Target;
-use lsp_server::{Message, Connection};
-use lsp_types::{InitializeParams, ServerCapabilities};
+use futures::future;
+use jsonrpc_core::{BoxFuture, Result};
+use serde_json::Value;
+use tower_lsp::{LanguageServer, LspService, Printer, Server};
+use tower_lsp::lsp_types::*;
 
-use secsp_language_server::server::Server;
+#[derive(Debug, Default)]
+struct Backend;
 
-pub mod query;
+impl LanguageServer for Backend {
+    type ShutdownFuture = BoxFuture<()>;
+    type SymbolFuture = BoxFuture<Option<Vec<SymbolInformation>>>;
+    type ExecuteFuture = BoxFuture<Option<Value>>;
+    type HoverFuture = BoxFuture<Option<Hover>>;
+    type HighlightFuture = BoxFuture<Option<Vec<DocumentHighlight>>>;
 
-fn main() -> Result<(), failure::Error> {
-    env_logger::builder().target(Target::Stderr).init();
-    let (connection, io_threads) = Connection::stdio();
+    fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::Incremental,
+                )),
+                hover_provider: Some(true),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec![".".to_string()]),
+                }),
+                signature_help_provider: Some(SignatureHelpOptions {
+                    trigger_characters: None,
+                }),
+                document_highlight_provider: Some(true),
+                workspace_symbol_provider: Some(true),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["dummy.do_something".to_string()],
+                }),
+                workspace: Some(WorkspaceCapability {
+                    workspace_folders: Some(WorkspaceFolderCapability {
+                        supported: Some(true),
+                        change_notifications: Some(
+                            WorkspaceFolderCapabilityChangeNotifications::Bool(true),
+                        ),
+                    }),
+                }),
+                ..ServerCapabilities::default()
+            },
+        })
+    }
 
-    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
-    let server_capabilities = serde_json::to_value(&ServerCapabilities::default()).unwrap();
-    let initialization_params = connection.initialize(server_capabilities)?;
+    fn initialized(&self, printer: &Printer, _: InitializedParams) {
+        printer.log_message(MessageType::Info, "server initialized!");
+    }
 
-    main_loop(serde_json::from_value(initialization_params).unwrap(), &connection)?;
-    io_threads.join()?;
-    Ok(())
+    fn shutdown(&self) -> Self::ShutdownFuture {
+        Box::new(future::ok(()))
+    }
+
+    fn did_change_workspace_folders(&self, printer: &Printer, _: DidChangeWorkspaceFoldersParams) {
+        printer.log_message(MessageType::Info, "workspace folders changed!");
+    }
+
+    fn did_change_configuration(&self, printer: &Printer, _: DidChangeConfigurationParams) {
+        printer.log_message(MessageType::Info, "configuration changed!");
+    }
+
+    fn did_change_watched_files(&self, printer: &Printer, _: DidChangeWatchedFilesParams) {
+        printer.log_message(MessageType::Info, "watched files have changed!");
+    }
+
+    fn symbol(&self, _: WorkspaceSymbolParams) -> Self::SymbolFuture {
+        Box::new(future::ok(None))
+    }
+
+    fn execute_command(&self, printer: &Printer, _: ExecuteCommandParams) -> Self::ExecuteFuture {
+        printer.log_message(MessageType::Info, "command executed!");
+        printer.apply_edit(WorkspaceEdit::default());
+        Box::new(future::ok(None))
+    }
+
+    fn did_open(&self, printer: &Printer, _: DidOpenTextDocumentParams) {
+        printer.log_message(MessageType::Info, "file opened!");
+    }
+
+    fn did_change(&self, printer: &Printer, _: DidChangeTextDocumentParams) {
+        printer.log_message(MessageType::Info, "file changed!");
+    }
+
+    fn did_save(&self, printer: &Printer, _: DidSaveTextDocumentParams) {
+        printer.log_message(MessageType::Info, "file saved!");
+    }
+
+    fn did_close(&self, printer: &Printer, _: DidCloseTextDocumentParams) {
+        printer.log_message(MessageType::Info, "file closed!");
+    }
+
+    fn hover(&self, _: TextDocumentPositionParams) -> Self::HoverFuture {
+        Box::new(future::ok(None))
+    }
+
+    fn document_highlight(&self, _: TextDocumentPositionParams) -> Self::HighlightFuture {
+        Box::new(future::ok(None))
+    }
 }
 
-fn main_loop(
-    params: InitializeParams,
-    connection: &Connection
-) -> Result<(), failure::Error> {
-    let server = Server::new(params);
-    server.run(connection)
+fn main() {
+    env_logger::init();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, messages) = LspService::new(Backend::default());
+    let handle = service.close_handle();
+    let server = Server::new(stdin, stdout)
+        .interleave(messages)
+        .serve(service);
+
+    tokio::run(handle.run_until_exit(server));
 }
--- packages/secsp-language-server/src/query.rs
+++
@@ -1,8 +0,0 @@
-use lsp_types::{Hover, HoverContents, MarkedString, TextDocumentPositionParams};
-
-pub fn hover_request(_params: TextDocumentPositionParams) -> Result<Option<Hover>, ()> {
-    Ok(Some(Hover {
-        contents: HoverContents::Scalar(MarkedString::from_markdown("test".to_owned())),
-        range: None,
-    }))
-}
